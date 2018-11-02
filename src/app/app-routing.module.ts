eacd3c16bb2437d63fa93143be0c4e942a033e40
ravi <linuxhome8@gmail.com>
2018-11-02 15:24:00 +0530

    updatinc ganges

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,18 @@ import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UniversalGuard } from './universal.guard';
+import { PolicyDetails1Component } from './policy-details1/policy-details1.component';
+import { ShowPolicyDetails1Component } from './show-policy-details1/show-policy-details1.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent, },
   {path: 'login/:msg', component: LoginComponent},
   {path: 'products', component: ContentComponent},
-  {path: 'history', component: ShowPolicyDetailsComponent, canActivate:[UniversalGuard]},
+  {path: 'history', component: PolicyDetails1Component, canActivate:[UniversalGuard], children:[
+    {path: 'health', component: ShowPolicyDetailsComponent},
+    {path: 'life', component: ShowPolicyDetails1Component}
+  ]},
   {path: 'advisors', component: ShowAdvisorsComponent},
   {path: 'quote', component: ShowQuoteComponent},
   {path: 'branches', component: SearchBranchComponent},
